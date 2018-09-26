ea53ee63497eb4b39fc3343b2172cde2bd2cad5f
David Jelenc <david.jelenc@fri.uni-lj.si>
2018-09-26 19:26:07 +0200

    Removes unused REALs, and simplifications

--- encode.c
+++ encode.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <sys/types.h>
-#include <Fault.h> /* Rectangle ASN.1 type */
+#include <Fault.h>
 
 /* Write the encoded output into some FILE stream. */
 static int write_out(const void *buffer, size_t size, void *app_key)
@@ -12,23 +12,21 @@ static int write_out(const void *buffer, size_t size, void *app_key)
 
 int main(int ac, char **av)
 {
-    Fault_t *instance; /* Type to encode */
-    asn_enc_rval_t ec;      /* Encoder return value */
-    /* Allocate the Fault_t */
+    Fault_t *instance;                     /* Type to encode */
+    asn_enc_rval_t ec;                     /* Encoder return value */
     instance = calloc(1, sizeof(Fault_t)); /* not malloc! */
     if (!instance)
     {
         perror("calloc() failed");
         exit(1);
     }
-    /* Initialize the Rectangle members */
-    PrintableString_t *message;
-    message = calloc(1, sizeof(PrintableString_t));
+
+    PrintableString_t *message = calloc(1, sizeof(PrintableString_t));
     message->buf = "Hello World!";
     message->size = 12;
 
-    instance->message = *message; //"Some error"; /* any random value */
-    instance->rid = 23;  /* any random value */
+    instance->message = *message;
+    instance->rid = 23;
     /* BER encode the data if filename is given */
     if (ac < 2)
     {
