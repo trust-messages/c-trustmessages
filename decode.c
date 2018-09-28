88b69d420a448cb3593d58e0fad6637696abd578
David Jelenc <david.jelenc@fri.uni-lj.si>
2018-09-28 16:46:45 +0200

    Preparing tests

--- decode.c
+++ decode.c
@@ -3,10 +3,40 @@
 #include <Query.h>
 #include <Fault.h>
 #include <Message.h>
+#include <time.h>
+
+/*
+Plan
+- create large messages (10 MB)
+- measure encoding / decoding speed
+- measure message size (be fair to XML?)
+- try BER / PER / XER
+*/
+
+
+void time_decode(Message_t *message, char *buf, size_t size)
+{
+    asn_dec_rval_t rval;
+    clock_t start, stop;
+
+    const uint64_t NUM = 100000000;
+    long double total = 0.0;
+
+    start = clock();
+    for (uint64_t i = 0; i < NUM; i++)
+    {
+        ber_decode(0, &asn_DEF_Message, (void **)&message, buf, size);
+    }
+    stop = clock();
+
+    total = (long double)(stop - start) / CLOCKS_PER_SEC;
+
+    printf("print(%Lf / %ld)\n", total, NUM);
+}
 
 int main(int ac, char **av)
 {
-    char buf[1024];         /* Temporary buffer */
+    char buf[8 * 1024];         /* Temporary buffer */
     asn_dec_rval_t rval;    /* Decoder return value */
     Message_t *message = 0; /* Type to decode. Note 0! */
     FILE *fp;               /* Input file handler */
@@ -46,8 +76,12 @@ int main(int ac, char **av)
                 filename, rval.consumed);
         exit(1);
     }
+
+    time_decode(message, buf, size);
+
     /* Print */
-    asn_fprint(stdout, &asn_DEF_Message, message);
+    // xer_fprint(stdout, &asn_DEF_Message, message);
+    // asn_fprint(stdout, &asn_DEF_Message, message);
     ASN_STRUCT_FREE(asn_DEF_Message, message);
     return 0;
 }
